f65beb244ede053e773f734310c3128191e57d94
jawadefaj <abjawad@ucsc.edu>
2023-12-28 17:46:23 -0800

    added yml, deleted citation.cff

--- nautilus/imagenet_download.py
+++ nautilus/imagenet_download.py
@@ -26,7 +26,7 @@ def download_file(url, filename):
 url = "https://storage.googleapis.com/kaggle-competitions-data/kaggle-v2/6799/4225553/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1704061320&Signature=TEYsta0BOIIfbTZLR%2FQgLAP1IkSd6qUNs%2BEETbdx2yDGsZaBdFm4rdjhO5anvFUksnTuaGs2ar99vV2SG8SeHwxlD6GiSfNYzVay2FmKwJ36bqwQGrXPSlF0Cb3ncIeI0epFNtTInd9PrkMKC9t0LZZQ%2F9zleUU7qlpOuAUex6CyiL8JM%2Bt6d6PLmBUoNUK6Tp9OBgPvnZITXZarpSOLp6w6b6Zct3hidgzyfFt8mYGwEpicDUvbuDQYMS1qsrqL5w9KK%2BLEX7XwaN2T2ZBkd2%2BR5PAX3DNcCI7U8SYXsjFEmUMNQS6B%2BPvprFSKDlfLhJbBuLZB9uoqEcAjCqB22Q%3D%3D&response-content-disposition=attachment%3B+filename%3Dimagenet-object-localization-challenge.zip"
 
 # Define the path where the dataset will be saved
-path = "/home/abjawad/Documents"
+path = "/project"
 filename = os.path.join(path, "imagenet-object-localization-challenge.zip")
 
 # Download the dataset
