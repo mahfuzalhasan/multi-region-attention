6898e9d7471e0767da831d13d6c18e091b781513
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-01-12 21:41:55 -0500

    config fix

--- configs/config_imagenet.py
+++ configs/config_imagenet.py
@@ -21,7 +21,7 @@ C.SYSTEM.abs_dir = osp.realpath(".")
 #C.SYSTEM.device_ids = [0] # for mahdi (lab-pc)
 # C.SYSTEM.device_ids = [0, 1] # for mahdi (newton)
 # C.SYSTEM.device_ids = [0, 1, 2, 3] # for FICS
-C.SYSTEM.device_ids = [0, 1, 2, 3, 4, 5, 6] # for nautilus
+C.SYSTEM.device_ids = [0, 1, 2, 3, 4, 5] # for nautilus
 
 
 """Dataset Config"""
@@ -43,7 +43,7 @@ C.DATASET.CACHE_MODE = 'part'
 
 ####################
 # Batch size for a single GPU, could be overwritten by command line argument
-C.DATASET.BATCH_SIZE = 128
+C.DATASET.BATCH_SIZE = 768
 # Path to dataset, could be overwritten by command line argument
 C.DATASET.DATA_PATH = ''
 # Dataset name
