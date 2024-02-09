valiation calculation fixed

validation.py
@@ -148,9 +148,9 @@ def val_imagenet(epoch, data_loader, model, config):
     model.eval()
 
     batch_time = AverageMeter()
-    # loss_meter = AverageMeter()
-    # acc1_meter = AverageMeter()
-    # acc5_meter = AverageMeter()
+    loss_meter = AverageMeter()
+    acc1_meter = AverageMeter()
+    acc5_meter = AverageMeter()
 
     top_1_total = 0
     top_5_total = 0
@@ -170,10 +170,18 @@ def val_imagenet(epoch, data_loader, model, config):
         loss = criterion(output, target)
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
 
+        #### calculation with avg_meter
+        total_loss += loss.item() * images.size(0)
+        acc1 = reduce_tensor(acc1)
+        acc5 = reduce_tensor(acc5)
+        loss = reduce_tensor(loss)
+        loss_meter.update(loss.item(), target.size(0))
+        acc1_meter.update(acc1.item(), target.size(0))
+        acc5_meter.update(acc5.item(), target.size(0))
+
+        total += 1
         top_1_total += acc1.item()
         top_5_total += acc5.item()
-        total_loss += loss.item() * images.size(0)
-        total += images.size(0)
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -182,47 +190,61 @@ def val_imagenet(epoch, data_loader, model, config):
         if idx % config.EVAL.EVAL_PRINT_FREQ == 0:
             memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
 
-            batch_total_loss_tensor = torch.tensor([total_loss], device=dist.get_rank())
+            # top_1_batch = (top_1_total / total)
+            # top_5_batch = (top_5_total / total)
+            
+            # batch_total_loss_tensor = torch.tensor([total_loss], device=dist.get_rank())
             batch_total_tensor = torch.tensor([total], device=dist.get_rank())
-            batch_top1_tensor = torch.tensor([top_1_total], device=dist.get_rank())
-            batch_top5_tensor = torch.tensor([top_5_total], device=dist.get_rank())
+            csum_batch_top1_tensor = torch.tensor([top_1_total], device=dist.get_rank())
+            csum_batch_top5_tensor = torch.tensor([top_5_total], device=dist.get_rank())
 
             # Aggregate counts across all GPUs
-            dist.all_reduce(batch_total_loss_tensor, op=dist.ReduceOp.SUM)
             dist.all_reduce(batch_total_tensor, op=dist.ReduceOp.SUM)
-            dist.all_reduce(batch_top1_tensor, op=dist.ReduceOp.SUM)
-            dist.all_reduce(batch_top5_tensor, op=dist.ReduceOp.SUM)
+            dist.all_reduce(csum_batch_top1_tensor, op=dist.ReduceOp.SUM)
+            dist.all_reduce(csum_batch_top5_tensor, op=dist.ReduceOp.SUM)
+            ## All of these should have been divided by dist.get_world_size()
+            ## but during accuracy calcuation we are taking ratio.
+            ## So, the effect of not dividing by 4 gets nullified eventually 
 
-            batch_avg_loss = batch_total_loss_tensor.item() / batch_total_tensor.item()
-            batch_top1_accuracy = batch_top1_tensor.item() / batch_total_tensor.item() * 100
-            batch_top5_accuracy = batch_top5_tensor.item() / batch_total_tensor.item() * 100
+
+            ravg_batch_top1_accuracy = csum_batch_top1_tensor.item() / batch_total_tensor.item()
+            ravg_batch_top5_accuracy = csum_batch_top5_tensor.item() / batch_total_tensor.item()
 
             if dist.get_rank()==0:
-                print(
-                    f'Test: [{idx}/{len(data_loader)}]\t'
+                print(                                                  ## print from manual calc
+                    f'#From Manual Calc:: Test: [{idx}/{len(data_loader)}]\t'
                     f'Batch Time Avg ({batch_time.avg:.3f})\t'
-                    f'Loss avg  ({batch_avg_loss:.4f})\t'
-                    f'Acc@1 avg ({batch_top1_accuracy:.3f})\t'
-                    f'Acc@5 avg ({batch_top5_accuracy:.3f})\t'
+                    f'Acc@1 avg ({ravg_batch_top1_accuracy:.3f})\t'
+                    f'Acc@5 avg ({ravg_batch_top5_accuracy:.3f})\t'
                     f'Mem {memory_used:.0f}MB')
-    ###### validation done ########  
-    total_loss_tensor = torch.tensor([total_loss], device=dist.get_rank())
-    total_tensor = torch.tensor([total], device=dist.get_rank())
-    top1_tensor = torch.tensor([top_1_total], device=dist.get_rank())
-    top5_tensor = torch.tensor([top_5_total], device=dist.get_rank())
+                print(                                                  ## print from AverageMeter
+                    f'#From AverageMeter::::: \t'
+                    f'Loss avg  ({loss_meter.avg:.4f})\t'
+                    f'Acc@1 avg ({acc1_meter.avg:.3f})\t'
+                    f'Acc@5 avg ({acc5_meter.avg:.3f})\t')
+
+    # batch_total_loss_tensor = torch.tensor([total_loss], device=dist.get_rank())
+    batch_total_tensor = torch.tensor([total], device=dist.get_rank())
+    csum_batch_top1_tensor = torch.tensor([top_1_total], device=dist.get_rank())
+    csum_batch_top5_tensor = torch.tensor([top_5_total], device=dist.get_rank())
 
     # Aggregate counts across all GPUs
-    dist.all_reduce(total_loss_tensor, op=dist.ReduceOp.SUM)
-    dist.all_reduce(total_tensor, op=dist.ReduceOp.SUM)
-    dist.all_reduce(top1_tensor, op=dist.ReduceOp.SUM)
-    dist.all_reduce(top5_tensor, op=dist.ReduceOp.SUM)
+    dist.all_reduce(batch_total_tensor, op=dist.ReduceOp.SUM)
+    dist.all_reduce(csum_batch_top1_tensor, op=dist.ReduceOp.SUM)
+    dist.all_reduce(csum_batch_top5_tensor, op=dist.ReduceOp.SUM)
+    ## All of these should have been divided by dist.get_world_size()
+    ## but during accuracy calcuation we are taking ratio.
+    ## So, the effect of not dividing by 4 gets nullified eventually 
+    ##########################################################
+
 
     if dist.get_rank() == 0:  # Optionally, only on the master node
-        avg_loss = total_loss_tensor.item() / total_tensor.item()
-        top1_accuracy = top1_tensor.item() / total_tensor.item() * 100
-        top5_accuracy = top5_tensor.item() / total_tensor.item() * 100
-        print(f'Validation Loss: {avg_loss:.4f}, Top-1 Accuracy: {top1_accuracy:.2f}%, Top-5 Accuracy: {top5_accuracy:.2f}%')
-        return top1_accuracy, top5_accuracy, avg_loss
+        top1_accuracy = csum_batch_top1_tensor.item() / batch_total_tensor.item()
+        top5_accuracy = csum_batch_top5_tensor.item() / batch_total_tensor.item()
+
+        print(f'Validation Loss: {loss_meter.avg:.4f}, Top-1 Accuracy: {top1_accuracy:.2f}%, Top-5 Accuracy: {top5_accuracy:.2f}%')
+        print(f'accuracy from avg meter::: acc1:{acc1_meter.avg:.3f} acc5:{acc5_meter.avg:.3f}')
+        return top1_accuracy, top5_accuracy, loss_meter.avg
     else:
         return None, None, None
     